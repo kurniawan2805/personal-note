e7ae2267992fad1b254a24609ea90929402b0551
Adi Kurniawan <42286406+kurniawan2805@users.noreply.github.com>
2022-08-21 13:32:42 +0700

    remove reactIcon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { MdSearch } from "react-icons/md";
 
 function Header({ handleSearchNote }) {
   // console.log(props);
@@ -7,11 +6,9 @@ function Header({ handleSearchNote }) {
     <div className="note-app__header">
       <h1>Notes</h1>
       <div className="note-search">
-        {/* <MdSearch className="search-icons" size="1.5em" /> */}
         <input
           type="text"
           placeholder="Cari catatan ..."
-          // value=""
           onChange={(e) => handleSearchNote(e.target.value)}
         />
       </div>
